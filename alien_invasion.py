6a7d497203fcfa731b234ca8f5e8a706ffcbbc15
eSam0531 <esama01@jaguar.tamu.edu>
2021-03-18 22:17:20 -0500

    left,right,quit keypress/release events with continuous movement, adjusted ships speed, limited ship's range to window, and refactored check events

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,11 +33,12 @@ class AlienInvasion:
         self.bg_color = (self.settings.bg_color)
         # Create an instance of the ship
         self.ship = Ship(self)
-
+        
     def run_game(self):
         'Start the main loop for the game.'
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             
     def _check_events(self):
@@ -45,6 +46,28 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self,event):
+        '''Respond to keypresses.'''
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right.
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Move the ship to the left.
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self,event):
+        '''Respond to key releases.'''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         '''Update images on the screen, and flip tot he new screen.'''
