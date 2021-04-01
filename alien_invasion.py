b70116ce51f68647a21028cda7be6ae50594d34f
eSam0531 <esama01@jaguar.tamu.edu>
2021-03-31 22:25:25 -0500

    added highscores, levels, and number of ships left

--- alien_invasion.py
+++ alien_invasion.py
@@ -89,6 +89,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -148,14 +150,20 @@ class AlienInvasion:
                     self.aliens, True, True)
         
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _create_fleet(self):
         '''Create the fleet of aliens.'''
@@ -217,8 +225,9 @@ class AlienInvasion:
     def _ship_hit(self):
         '''Respond to the ship being hit by an alien'''
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             #Get rid of any remaining aliens and bullets.
             self.aliens.empty()
--- settings.py
+++ settings.py
@@ -25,6 +25,8 @@ class Settings:
 
         #How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the laien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -41,7 +43,11 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        '''increase speed settings.'''
+        '''increase speed settings and alien point values.'''
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # used to test that points were increasing with each level
+        #print(self.alien_points)
